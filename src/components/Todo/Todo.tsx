ce12a9489c744dd22c48a5f8318570647a66b0a9
shyptia <shyptianastya27@gmail.com>
2024-04-28 14:06:34 +0300

    add ability to change todo title

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -3,7 +3,11 @@ import { Todo as TodoType } from "../../redux/todos/types";
 import { useDispatch } from "react-redux";
 import { deleteTodo, updateTodo } from "../../redux/todos/actions";
 import deleteIcon from "../../assets/svg/delete-icon.svg";
+import editIcon from "../../assets/svg/edit-icon.svg";
 import "./styles.scss";
+import { useState } from "react";
+import { TextInput } from "../TextInput";
+import { Button } from "../Button";
 
 export function Todo({
   todo,
@@ -13,15 +17,24 @@ export function Todo({
   deleted?: boolean;
 }) {
   const dispatch = useDispatch();
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(todo.title);
 
   const handleDeleteTodo = () => {
-    if (deleted) return;
-
     dispatch(deleteTodo(todo));
   };
 
   const handleCompletedChange = () => {
     dispatch(updateTodo({ ...todo, completed: !todo.completed }));
+  };
+
+  const handleChangeTitle = () => {
+    if (!editedTitle.trim()) {
+      return;
+    }
+
+    dispatch(updateTodo({ ...todo, title: editedTitle }));
+    setIsEditing(false);
   };
 
   return (
@@ -30,17 +43,43 @@ export function Todo({
         type="checkbox"
         checked={todo.completed}
         onChange={handleCompletedChange}
-        disabled={deleted}
+        disabled={deleted || isEditing}
       />
-      <p className="todo__title">{todo.title}</p>
+      {isEditing ? (
+        <div className="edit-container">
+          <TextInput
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)}
+          />
+          <Button onClick={handleChangeTitle}>Save changes</Button>
+        </div>
+      ) : (
+        <p
+          className={clsx(
+            "todo__title",
+            todo.completed && "todo__title_completed"
+          )}
+        >
+          {todo.title}
+        </p>
+      )}
 
-      {!deleted && (
-        <img
-          className="todo__delete-icon"
-          onClick={handleDeleteTodo}
-          src={deleteIcon}
-          alt="logo"
-        />
+      {!deleted && !isEditing && (
+        <>
+          <img
+            className="todo__delete-icon"
+            onClick={() => setIsEditing(true)}
+            src={editIcon}
+            alt="edit icon"
+          />
+
+          <img
+            className="todo__delete-icon"
+            onClick={handleDeleteTodo}
+            src={deleteIcon}
+            alt="delete icon"
+          />
+        </>
       )}
     </div>
   );
